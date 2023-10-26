2cda51c677e5a74e20c2d1f2cc9f7f4dadf58d4b
Kayho-Rose <imandmov@gmail.com>
2023-10-26 18:42:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
-const database = getDatabase(firebaseConfig.databaseURL)
+const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
 
 /* === UI === */
